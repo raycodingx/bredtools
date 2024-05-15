788a57e81e63c146b88a79f021f3578fdb399156
raycodingx <raycodingx@gmail.com>
2024-05-15 16:23:37 +0800

    modified sortped func

--- R/sortped.R
+++ R/sortped.R
@@ -9,9 +9,10 @@
 #' @param addgen A logical value indicates whether the individual generation number will be inferred. The default values is TRUE, then a new column named \strong{Gen} will be added in the returned data.table.
 #'
 #' @return A data.table including the resorted pedigree is returned. The individual generation is inferred and a new column \strong{Gen} is added when the parameter \emph{addgen} is TRUE. The Gen column is integer.
-#' @keywords internal
+#'
 #' @import data.table
 #' @export
+#'
 sortped <- function(ped,addgen=TRUE) {
   ped_new <- copy(ped)
   ped_new_colnames <- colnames(ped_new)
